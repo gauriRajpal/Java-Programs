35fcdc30c9226e8e11a47423d13541767bad1566
Gauri Rajpal <gaurirajpal2005@gmail.com>
2024-07-12 01:26:24 +0530

    To print all the elements of the array

---
+++ Pro_print_array.java
@@ -0,0 +1,9 @@
+public class Pro_print_array {
+    public void main(String[] args){
+        int[] a = {34,67,55,23};
+        for(int i = a.length-1;i>=0;i--){
+            System.out.print(a[i]);
+            System.out.print( " ");
+        }
+    }
+}
